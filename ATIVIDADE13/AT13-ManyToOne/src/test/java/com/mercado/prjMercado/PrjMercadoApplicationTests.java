b56e1d7143c7a205422e524ac5251fc07db7554a
Rafaela Silva da Paz <159180376+Blightpb@users.noreply.github.com>
2024-11-19 07:53:45 -0300

    Add files via upload

---
+++ ATIVIDADE13/AT13-ManyToOne/src/test/java/com/mercado/prjMercado/PrjMercadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercado.prjMercado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjMercadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
